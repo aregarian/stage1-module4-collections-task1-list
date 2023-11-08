9eaa220cdc48345fbf57c61c9a732fd57266efcd
Fanny Dorthey <fannydor@un-k4.kzn.21-school.ru>
2023-11-08 05:02:56 +0300

    hehe boy

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> hehe = new ArrayList<>();
+        for (int i = 1; i <= sourceList.size()  ; i++) {
+            if (i % 3 == 0) {
+                hehe.add(sourceList.get(i - 1));
+                hehe.add(sourceList.get(i - 1));
+            }
+        }
+        return hehe;
     }
 }
